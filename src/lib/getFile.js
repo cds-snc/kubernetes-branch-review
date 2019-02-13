ee5fd4d66b668e1452ca0220bf2db4244eaf9328
Tim Arney <tim@line37.com>
2019-02-13 12:07:11 -0500

    initial octokit

---
+++ src/lib/getFile.js
@@ -0,0 +1,10 @@
+import fs from "fs";
+
+export const getFile = async file => {
+  return new Promise((resolve, reject) => {
+    fs.readFile(file, (err, data) => {
+      if (err) reject(err);
+      resolve(data);
+    });
+  });
+};
---
+++ src/lib/index.js
@@ -0,0 +1,3 @@
+import octokit from "@octokit/rest";
+export { notify } from "./githubNotify";
+export default octokit();
