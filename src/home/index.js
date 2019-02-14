dd0ed14a66e9ce209bf1d2205cef77dc11359c5b
Tim Arney <tim@line37.com>
2019-02-14 10:56:55 -0500

    initial route setup

--- src/home/index.js
+++ src/home/index.js
@@ -1,1 +1,21 @@
-export { router as homeRouter } from "./router";
+import express from "express";
+
+const router = express.Router();
+
+router.get("/favicon.ico", (req, res) => res.status(204));
+
+router.get("/", async (req, res) => {
+  const action = req.action;
+  switch (action) {
+    case "opened":
+      res.redirect("/create");
+      break;
+    case "push":
+      res.redirect("/push");
+      break;
+    default:
+      res.send("");
+  }
+});
+
+export default router;
--- src/server.js
+++ src/server.js
@@ -1,9 +1,13 @@
 import express from "express";
-import { homeRouter } from "./home";
+import homeRouter from "./home";
+import createRouter from "./create";
+import pushRouter from "./push";
 const port = parseInt(process.env.PORT, 10) || 4000;
 const server = express();
 
 server.use("/", homeRouter);
+server.use("/create", createRouter);
+server.use("/push", pushRouter);
 
 server.listen(port, err => {
   if (err) throw err;
