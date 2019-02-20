3c81eb85eac7efbb4d8a00bbc5541756acbad6c6
Tim Arney <tim@line37.com>
2019-02-20 09:26:41 -0500

    initial digital ocean api setup

--- src/create/createCluster.js
+++ src/create/createCluster.js
@@ -1,5 +1,6 @@
 import { saveReleaseToDB } from "../db/queries";
-import { createDeployment } from "../lib/githubNotify";
+// import { createDeployment } from "../lib/githubNotify";
+import { create } from "../api";
 
 export const createCluster = async (req, res) => {
   const body = req.body;
@@ -12,10 +13,14 @@ export const createCluster = async (req, res) => {
       cluster_state: "in_progress"
     });
 
-    const result = await createDeployment(req.body);
+    // const result = await createDeployment(req.body);
+    create();
+    const result = "hello";
+
     // api call to digital ocean ...
     return result;
   } catch (e) {
+    console.log(e);
     console.log("err", e.message);
   }
 };
