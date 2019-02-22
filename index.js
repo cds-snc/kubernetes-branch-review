0852bc42f4d7182121a9ef05aca8a481ec39d3a0
Max <max@neuvians.net>
2019-02-22 10:17:58 -0500

    Traefik added

--- index.js
+++ index.js
@@ -1,3 +1,3 @@
 require = require("esm")(module); // eslint-disable-line no-global-assign
-require("dotenv-safe").config();
+require("dotenv-safe").config({ allowEmptyValues: true });
 module.exports = require("./src/server.js");
