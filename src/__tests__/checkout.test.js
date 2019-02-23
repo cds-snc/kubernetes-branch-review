360cd9b7f8fee29329776dbb2bb4610c11f9620d
Max Neuvians <max@neuvians.net>
2019-02-23 09:44:49 -0500

    Added checkout code (#11)
    * Added checkout code
    
    * Default code dir'
    
    * Update main.workflow
    
    * Update main.workflow

---
+++ src/__tests__/checkout.test.js
@@ -0,0 +1,24 @@
+import { checkout } from "../lib/checkout";
+const { spawnSync } = require("child_process");
+
+describe("checkoutRepo", () => {
+  it("returns false if the repo does not exist", async () => {
+    expect(await checkout("foo", "abcd")).toEqual(false);
+  });
+
+  it("returns false if it is able to checkout the repo but the sha does not exist", async () => {
+    expect(await checkout("cds-snc/dns", "abcd")).toEqual(false);
+    spawnSync("rm", ["-rf", "dns"], {
+      cwd: "/tmp"
+    });
+  });
+
+  it("returns true if it is able to checkout the repo and the sha", async () => {
+    expect(
+      await checkout("cds-snc/dns", "686880e3ba2b2d782de57ff1540cdb00c7eb774a")
+    ).toEqual(true);
+    spawnSync("rm", ["-rf", "dns"], {
+      cwd: process.env.CODE_DIR
+    });
+  });
+});
