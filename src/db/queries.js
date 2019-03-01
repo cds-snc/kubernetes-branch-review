9161555f7f0063f17c8add3fbdbfaf2da9fe12ea
Tim Arney <timarney@users.noreply.github.com>
2019-03-01 09:10:38 -0500

    add server tests (#20)

--- src/db/queries.js
+++ src/db/queries.js
@@ -19,6 +19,7 @@ export const getRelease = async (obj, query = { refId: obj.refId }) => {
   await dbConnect();
   try {
     let record = await Model.findOne(query).exec();
+    if (!record || !record.refId) return false;
     record.full_name = getFullNameFromRefId(record.refId);
     return record;
   } catch (e) {
