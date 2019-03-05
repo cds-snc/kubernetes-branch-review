bbb464750d04e4ccdc9337c2b22e4331a538d111
Tim Arney <timarney@users.noreply.github.com>
2019-03-05 14:03:58 -0500

    save ip and update (#28)

---
+++ src/lib/saveIp.js
@@ -0,0 +1,26 @@
+import { getRelease, saveReleaseToDB } from "../db/queries";
+import { getLoadBalancerIp } from "../lib/getLoadBalancer";
+import { updateDeployment, updateDeploymentStatus } from "../lib/githubNotify";
+
+export const saveIp = async ({ refId }) => {
+  const record = await getRelease({ refId });
+  const clusterId = record.cluster_id;
+  const ip = await getLoadBalancerIp(clusterId);
+  await saveReleaseToDB({ refId }, { load_balancer_ip: ip });
+  return ip;
+};
+
+export const saveIpAndUpdate = async (req, sha, refId) => {
+  const ip = await saveIp({ refId });
+  const deployment = await updateDeployment(req, sha);
+
+  if (!deployment || !deployment.data || !deployment.data.id) {
+    return false;
+  }
+
+  await updateDeploymentStatus(req, {
+    log_url: `http://${ip}`,
+    target_url: `http://${ip}`,
+    deployment_id: deployment.data.id
+  });
+};
