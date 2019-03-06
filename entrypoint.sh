eda4c3e98f55690256645b659ff501e041d6bf6b
Max Neuvians <max@neuvians.net>
2019-03-06 10:42:04 -0500

    Specify docker login server

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,5 +2,5 @@
 set -e
 
 sh -c "dockerd --host=unix:///var/run/docker.sock --host=tcp://0.0.0.0:2375 &"
-sh -c 'docker login -u elenchosclient -p "$(cat docker.txt)"'
+sh -c 'docker login -u elenchosclient -p "$(cat docker.txt)" https://index.docker.io/v1'
 sh -c "yarn start"
