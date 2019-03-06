8cc38e2ed3f240d427cb0301b2c09104ec408adf
Tim Arney <tim@line37.com>
2019-03-06 10:53:57 -0500

    add poll load balancer

--- src/lib/saveIp.js
+++ src/lib/saveIp.js
@@ -1,10 +1,14 @@
 import { getRelease, saveReleaseToDB } from "../db/queries";
 import { getLoadBalancerIp } from "../lib/getLoadBalancer";
+import { pollLoadBalancer } from "../lib/pollLoadBalancer";
 import { updateDeployment, updateDeploymentStatus } from "../lib/githubNotify";
 
 export const saveIp = async ({ refId }) => {
   const record = await getRelease({ refId });
   const clusterId = record.cluster_id;
+
+  await pollLoadBalancer(clusterId);
+
   const ip = await getLoadBalancerIp(clusterId);
   await saveReleaseToDB({ refId }, { load_balancer_ip: ip });
   return ip;
