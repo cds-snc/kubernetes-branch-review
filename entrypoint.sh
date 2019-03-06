14028dcb48be73caf4b4ad49721cbb70c76aca40
Max Neuvians <max@neuvians.net>
2019-03-06 12:06:27 -0500

    Add insecure registry to docker daemon`

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 set -e
 
-sh -c "dockerd --host=unix:///var/run/docker.sock --host=tcp://0.0.0.0:2375 &"
+sh -c "dockerd --host=unix:///var/run/docker.sock --host=tcp://0.0.0.0:2375 --insecure-registry=registry:5000 &"
 sh -c "yarn start"
