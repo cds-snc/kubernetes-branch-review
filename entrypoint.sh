7f717928d3435f0dfce7ef2e3c1397e0e848837a
Max Neuvians <max@neuvians.net>
2019-03-06 18:39:18 -0500

    Use another gcr.io repo

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,4 +2,5 @@
 set -e
 
 sh -c "dockerd --host=unix:///var/run/docker.sock --host=tcp://0.0.0.0:2375 --insecure-registry=registry:5000 &"
+sh -c 'docker login -u _json_key -p "$(cat gcloud.json)" https://gcr.io'
 sh -c "yarn start"
