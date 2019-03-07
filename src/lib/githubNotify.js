1e9e42fef030586ac43c3d7fb8ce397f9c53d550
Tim Arney <timarney@users.noreply.github.com>
2019-03-07 12:45:26 -0500

    create deployment status (#31)

--- src/lib/githubNotify.js
+++ src/lib/githubNotify.js
@@ -1,4 +1,5 @@
 import { authenticate } from "./githubAuth";
+import { getDeployment } from "../db/queries";
 export const createDeployment = async (
   event,
   status = { task: "deploy", description: "Initializing deployment" }
@@ -30,52 +31,25 @@ export const createDeployment = async (
   return result;
 };
 
-export const updateDeployment = async (
-  event,
-  sha,
-  status = { task: "deploy", description: "Updating deployment" }
-) => {
-  const client = await authenticate(event.installation.id);
-  const repoOwner = event.repository.owner.login;
-  const repoName = event.repository.name;
-
-  const statusObj = Object.assign(
-    {
-      owner: repoOwner,
-      repo: repoName,
-      ref: sha,
-      environment: "staging",
-      payload: "from the app"
-    },
-    status
-  );
-
-  let result = "";
-
-  try {
-    result = await client.repos.createDeployment(statusObj);
-  } catch (e) {
-    console.log(e.message);
-  }
-
-  return result;
-};
-
 export const updateDeploymentStatus = async (
   event,
-  status = { target_url: "" }
+  status = { state: "success", description: "deployment updated" },
+  refId
 ) => {
+  const deployment = getDeployment({ refId: refId });
   const client = await authenticate(event.installation.id);
   const repoOwner = event.repository.owner.login;
   const repoName = event.repository.name;
+  const ip = deployment.ip;
 
   const statusObj = Object.assign(
     {
       owner: repoOwner,
       repo: repoName,
-      state: "success",
       environment: "staging",
-      description: "deployment updated"
+      deployment_id: deployment.id,
+      log_url: `http://${ip}`,
+      environment_url: `http://${ip}`
     },
     status
   );
--- src/lib/saveIp.js
+++ src/lib/saveIp.js
@@ -1,12 +1,11 @@
 import { getRelease, saveReleaseToDB } from "../db/queries";
 import { getLoadBalancerIp } from "../lib/getLoadBalancer";
 import { pollLoadBalancer } from "../lib/pollLoadBalancer";
-import { updateDeployment, updateDeploymentStatus } from "../lib/githubNotify";
+import { updateDeploymentStatus } from "../lib/githubNotify";
 
 export const saveIp = async ({ refId }) => {
   const record = await getRelease({ refId });
   const clusterId = record.cluster_id;
-
   await pollLoadBalancer(clusterId);
 
   const ip = await getLoadBalancerIp(clusterId);
@@ -15,16 +14,6 @@ export const saveIp = async ({ refId }) => {
 };
 
 export const saveIpAndUpdate = async (req, sha, refId) => {
-  const ip = await saveIp({ refId });
-  const deployment = await updateDeployment(req, sha);
-
-  if (!deployment || !deployment.data || !deployment.data.id) {
-    return false;
-  }
-
-  await updateDeploymentStatus(req, {
-    log_url: `http://${ip}`,
-    environment_url: `http://${ip}`,
-    deployment_id: deployment.data.id
-  });
+  await saveIp({ refId });
+  await updateDeploymentStatus(req, {}, refId);
 };
