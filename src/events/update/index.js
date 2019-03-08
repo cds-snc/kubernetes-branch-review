9542ba32875fd426775a3d88f526993083211f22
Tim Arney <tim@line37.com>
2019-03-08 10:31:20 -0500

    fix deploy updates

--- src/events/update/index.js
+++ src/events/update/index.js
@@ -1,8 +1,11 @@
 import { getRefId } from "../../lib/getRefId";
 import { getRelease, saveReleaseToDB } from "../../db/queries";
-import { updateDeploymentStatus } from "../../lib/githubNotify";
+import {
+  updateDeploymentStatus,
+  createDeployment
+} from "../../lib/githubNotify";
 
-export const update = async (req, release) => {
+export const update = async req => {
   const body = req.body;
   const sha = body.after;
   const refId = getRefId(body);
@@ -16,6 +19,25 @@ export const update = async (req, release) => {
     record = await getRelease({ refId });
   }
 
+  // mark old deployment as inactive (just in case)
+  // note: this should happen automatically in most cases when a status is updated
+  // https://octokit.github.io/rest.js/#api-Repos-createDeploymentStatus
+
+  await updateDeploymentStatus(
+    req,
+    { state: "inactive", description: "closed deployment" },
+    refId
+  );
+
+  // create a new deployment
+  const deployment = await createDeployment(body);
+
+  await saveReleaseToDB({
+    refId,
+    deployment_id: deployment.id
+  });
+
+  // set deployment to in progress
   await updateDeploymentStatus(
     req,
     { state: "in_progress", description: "updating deployment..." },
