466e6d3fe1c7a5efdbfdb8b34222cf9b9820cdec
Tim Arney <timarney@users.noreply.github.com>
2019-04-26 10:30:39 -0400

    Add calls to update status in Github (#41)
    * update status
    
    * update tests

---
+++ src/lib/githubStatus.js
@@ -0,0 +1,58 @@
+/* https://octokit.github.io/rest.js/ */
+
+import { authenticate } from "./githubAuth";
+import { getDeployment } from "../db/queries";
+
+const validate = event => {
+  if (
+    !event ||
+    !event.repository ||
+    !event.repository.name ||
+    !event.repository.owner ||
+    !event.repository.owner.login
+  ) {
+    return false;
+  }
+
+  return true;
+};
+
+export const updateStatus = async (
+  event,
+  status = { state: "pending", description: "..." },
+  refId
+) => {
+  if (!validate(event)) return false;
+
+  if (!event || !event.installation) {
+    console.log("no event.installation");
+    return;
+  }
+
+  const client = await authenticate(event.installation.id);
+  const repoOwner = event.repository.owner.login;
+  const repoName = event.repository.name;
+
+  if (status.state === "success") {
+    const deployment = await getDeployment({ refId: refId });
+
+    if (deployment && deployment.ip) {
+      const ip = deployment.ip;
+      status.target_url = `http://${ip}`;
+    }
+  }
+
+  const statusObj = Object.assign(
+    {
+      owner: repoOwner,
+      repo: repoName,
+      sha: event.after,
+      context: "K8's branch deploy"
+    },
+    status
+  );
+
+  const result = await client.repos.createStatus(statusObj);
+
+  return result;
+};
