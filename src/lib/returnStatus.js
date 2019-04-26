8b086e9e71501e4f820fd0cb577e153eaa9e7e12
Tim Arney <tim@line37.com>
2019-04-26 10:17:38 -0400

    update status

--- src/lib/returnStatus.js
+++ src/lib/returnStatus.js
@@ -1,6 +1,10 @@
 import { updateDeploymentStatus } from "../lib/githubNotify";
+import { updateStatus } from "../lib/githubStatus";
+import { getRefId } from "../lib/getRefId";
 
 export const returnStatus = async (body, res, status) => {
-  await updateDeploymentStatus(body, status);
+  const refId = getRefId(body);
+  await updateDeploymentStatus(body, status, refId);
+  await updateStatus(body, status, refId);
   res.send(status.description);
 };
