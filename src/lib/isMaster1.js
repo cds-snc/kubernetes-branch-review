033a4d28b7ecf06af3b5525747773250a72780c6
Max <max@neuvians.net>
2019-04-29 12:01:48 -0400

    Removed old files

--- src/lib/isMaster1.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-exports.__esModule = true;
-exports.isMaster = function (event) {
-    return event && event.ref && event.ref.indexOf("master") !== -1;
-};
--- src/main.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-exports.__esModule = true;
-var isMaster_1 = require("./lib/isMaster");
-exports.isMaster = isMaster_1.isMaster;
-var getAction_1 = require("./lib/getAction");
-exports.getAction = getAction_1.getAction;
