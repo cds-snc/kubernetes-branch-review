7d4daf49b874b3f9681898430ada464076956cba
Tim Arney <tim@line37.com>
2019-04-29 14:22:31 -0400

    pollCluster

--- src/lib/pollCluster.ts
+++ src/lib/pollCluster.ts
@@ -1,7 +1,10 @@
 import { longPoll } from "../lib/longPoll";
 import { getCluster } from "../api";
 
-export const pollCluster = async (clusterId, checkState = "running", cb) => {
+export const pollCluster = async (
+  clusterId: string,
+  checkState: string = "running"
+): Promise<void> => {
   return new Promise(resolve => {
     const poll = Object.assign({}, longPoll);
     const prefix = "cluster";
@@ -9,10 +12,9 @@ export const pollCluster = async (clusterId, checkState = "running", cb) => {
     poll.id = id;
     poll.delay = 20000;
 
-    poll.check = async () => {
+    poll.check = async (): Promise<void> => {
       const result = await getCluster(clusterId);
       const clusterState = result.kubernetes_cluster.status.state;
-
       console.log(`current cluster state ... ${clusterState}`);
 
       if (clusterState === checkState) {
