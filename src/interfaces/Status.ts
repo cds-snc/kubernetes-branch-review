3eb87b3b74eaad31206cf82939933b9165289e01
Tim Arney <tim@line37.com>
2019-04-29 12:13:24 -0400

    add return status

---
+++ src/interfaces/Status.ts
@@ -0,0 +1,4 @@
+export interface Status {
+  state: string;
+  description: string;
+}
--- src/lib/returnStatus.ts
+++ src/lib/returnStatus.ts
@@ -1,8 +1,15 @@
 import { updateDeploymentStatus } from "../lib/githubNotify";
 import { updateStatus } from "../lib/githubStatus";
 import { getRefId } from "../lib/getRefId";
+import { RequestBody } from "../interfaces/Request";
+import { Status } from "../interfaces/Status";
+import { Response } from "express";
 
-export const returnStatus = async (body, res, status) => {
+export const returnStatus = async (
+  body: RequestBody,
+  res: Response,
+  status: Status
+) => {
   const refId = getRefId(body);
   await updateDeploymentStatus(body, status, refId);
   await updateStatus(body, status, refId);
