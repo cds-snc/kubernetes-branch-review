517b6407b0732311a3ff0ed0f4d18687bc31ef0c
Max <max@neuvians.net>
2019-04-30 09:49:25 -0400

    Added getRefId

--- src/interfaces/Request.ts
+++ src/interfaces/Request.ts
@@ -1,8 +1,21 @@
+export interface Head {
+  ref: string;
+}
+
+export interface PullRequest {
+  head: Head;
+}
+
+export interface Repository {
+  full_name: string;
+}
+
 export interface RequestBody {
   action: string;
-  repository: string;
+  repository: Repository;
   ref: string;
-  after?:string
+  after?:string;
+  pull_request: PullRequest;
 }
 
 export interface Request {
--- src/lib/getRefId.ts
+++ src/lib/getRefId.ts
@@ -1,7 +1,8 @@
 import { isMaster } from "../lib/isMaster";
+import { RequestBody } from "../interfaces/Request";
 
-export const getRefId = event => {
-  let refId = false;
+export const getRefId = (event:RequestBody): false|string => {
+  let refId:string;
 
   if (!event || !event.repository || !event.repository.full_name) {
     return false;
@@ -12,7 +13,7 @@ export const getRefId = event => {
   if (event && event.action) {
     refId = event.pull_request.head.ref;
   } else if (event && event.ref) {
-    if (!isMaster(event)) {
+    if (!isMaster(event))event {
       const ref = event.ref.split("/");
       refId = ref[ref.length - 1];
     }
@@ -25,7 +26,7 @@ export const getRefId = event => {
   return `${fullName}/${refId}`;
 };
 
-export const getFullNameFromRefId = refId => {
+export const getFullNameFromRefId = (refId:string): false|string => {
   if (!refId) return false;
   let ref = refId.split("/");
   ref.pop();
