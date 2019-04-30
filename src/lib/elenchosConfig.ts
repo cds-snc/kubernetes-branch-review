71aa147443e493ed9654d3711880d2d6b16f630b
Tim Arney <timarney@users.noreply.github.com>
2019-04-30 08:19:51 -0400

    Kustomize ts (#44)
    * add long poll
    
    * add kustomize

--- src/lib/elenchosConfig.ts
+++ src/lib/elenchosConfig.ts
@@ -2,12 +2,12 @@ import { getFile } from "./getFile";
 const DIR = process.env.CODE_DIR || "/tmp";
 import { Config } from "../interfaces/Config";
 
-export const elenchosConfig = async (sha:string): Promise<false|Config> => {
+export const elenchosConfig = async (sha: string): Promise<false | Config> => {
   const configData = await getFile(`${DIR}/${sha}/elenchos.json`);
   if (!configData) return false;
 
   try {
-    const config = JSON.parse(configData);
+    const config = JSON.parse(configData.toString());
     if (!config["dockerfiles"] || !config["overlay"]) {
       console.log("Missing elenchos configuration variables");
       return false;
--- src/lib/kustomize.ts
+++ src/lib/kustomize.ts
@@ -1,26 +1,31 @@
 import { getFile } from "./getFile";
 import yaml from "js-yaml";
+import { Config } from "../interfaces/Config";
 
 const fs = require("fs");
 const DIR = process.env.CODE_DIR || "/tmp";
 
-const loadKustomization = async (sha, overlayPath) => {
+const loadKustomization = async (sha: string, overlayPath: string) => {
   const configData = await getFile(
     `${DIR}/${sha}/${overlayPath}/kustomization.yaml`
   );
   if (!configData) return false;
   try {
-    return yaml.safeLoad(configData, "utf8");
+    return yaml.safeLoad(configData.toString("utf8"));
   } catch (e) {
     console.error(e.message);
     return false;
   }
 };
 
-const writeKustomization = async (sha, overlayPath, config) => {
+const writeKustomization = async (
+  sha: string,
+  overlayPath: string,
+  config: Config
+): Promise<boolean> => {
   const filePath = `${DIR}/${sha}/${overlayPath}/kustomization.yaml`;
   try {
-    fs.writeFile(filePath, yaml.safeDump(config), "utf8", e =>
+    fs.writeFile(filePath, yaml.safeDump(config), "utf8", (e: Error) =>
       console.error(e)
     );
     return true;
@@ -30,7 +35,11 @@ const writeKustomization = async (sha, overlayPath, config) => {
   }
 };
 
-export const editKustomization = async (sha, overlayPath, images) => {
+export const editKustomization = async (
+  sha: string,
+  overlayPath: string,
+  images: {}[]
+): Promise<string | boolean> => {
   let config = await loadKustomization(sha, overlayPath);
   if (!config) return false;
   config.images = images;
