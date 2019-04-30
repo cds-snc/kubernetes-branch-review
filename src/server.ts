b6dd7c4bcda1cbc9d85a7b1d7a58b4f0d8218682
Tim Arney <tim@line37.com>
2019-04-30 10:26:05 -0400

    server ts

--- src/server.ts
+++ src/server.ts
@@ -2,14 +2,13 @@ import express from "express";
 import bodyParser from "body-parser";
 import homeRouter from "./routes";
 
-const port = parseInt(process.env.PORT, 10) || 4000;
+const defaultPort = parseInt(process.env.PORT, 10) || 4000;
 
 export const server = express();
 
 server.use(bodyParser.json());
 server.use("/", homeRouter);
-
-server.listen(port, err => {
+server.listen((port: number = defaultPort, err: Error) => {
   if (err) throw err;
   console.log(`> Ready on http://localhost:${port}`);
 });
