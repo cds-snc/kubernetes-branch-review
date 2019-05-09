2f5ce5f2d57e937fbf2ef11b4704114d184fbbb5
Tim Arney <tim@line37.com>
2019-05-09 08:42:00 -0400

    fix server  signature

--- src/server.ts
+++ src/server.ts
@@ -2,13 +2,14 @@ import express from "express";
 import bodyParser from "body-parser";
 import homeRouter from "./routes";
 
-const defaultPort = parseInt(process.env.PORT, 10) || 4000;
+const port = parseInt(process.env.PORT, 10) || 4000;
 
 export const server = express();
 
 server.use(bodyParser.json());
 server.use("/", homeRouter);
-server.listen((port: number = defaultPort, err: Error) => {
+
+server.listen(port, (err: Error) => {
   if (err) throw err;
   console.log(`> Ready on http://localhost:${port}`);
 });
